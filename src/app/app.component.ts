651e8fdeb7fcbba8ea919394a516e6ce8ffff3d7
karthikmohan26 <77399768+karthikmohan26@users.noreply.github.com>
2021-01-17 18:27:38 -0500

    Checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome Users';
+  title = 'Billinag and Payroll Login ';
 }
